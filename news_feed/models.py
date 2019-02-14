a69fd0487b4e2bd13831c114472664d79c7c5712
FShah1596 <42182692+FShah1596@users.noreply.github.com>
2019-02-14 11:35:40 -0800

    Update models.py

--- news_feed/models.py
+++ news_feed/models.py
@@ -2,36 +2,36 @@ from django.db import models
 import datetime
 
 
-# class TopHeadlines(models.Model):
-#     name = models.CharField(max_length=50)
-#     author = models.CharField(max_length=100)
-#     title = models.CharField(max_length=100)
-#     description = models.CharField(max_length=500)
-#     url = models.CharField(max_length=200)
-#     urlToImage = models.CharField(max_length=500, null=True)
-#     publishedAt = models.DateTimeField('date_published')
-#     content = models.CharField(max_length=1000)
-#
-#     class Meta:
-#             unique_together = ("name", "description", "url", "urlToImage", "publishedAt")
-#
-#     def __str__(self):
-#         return self.description + ',' + str(self.publishedAt)
-#
-#     def jsonToNewsModel(self, jsonData,i):
-#         # i=0
-#         # print (1)
-#         news = News()
-#         category = Categories.objects.get(pk='id')
-#         news.model = category
-#         news.name = jsonData['articles'][i]['source']['name']
-#         news.description = jsonData['articles'][i]['description']
-#         news.url = jsonData['articles'][i]['url']
-#         news.urlToImage = jsonData['articles'][i]['urlToImage']
-#         news.publishedAt = jsonData['articles'][i]['publishedAt']
-#         # print (i)
-#         return news
-        # i=i+1
+class TopHeadlines(models.Model):
+    name = models.CharField(max_length=50)
+    author = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    url = models.CharField(max_length=200)
+    urlToImage = models.CharField(max_length=500, null=True)
+    publishedAt = models.DateTimeField('date_published')
+    content = models.CharField(max_length=1000)
+
+    class Meta:
+            unique_together = ("name", "description", "url", "urlToImage", "publishedAt")
+
+    def __str__(self):
+        return self.description + ',' + str(self.publishedAt)
+
+    def jsonToNewsModel(self, jsonData,i):
+        # i=0
+        # print (1)
+        news = News()
+        category = Categories.objects.get(pk='id')
+        news.model = category
+        news.name = jsonData['articles'][i]['source']['name']
+        news.description = jsonData['articles'][i]['description']
+        news.url = jsonData['articles'][i]['url']
+        news.urlToImage = jsonData['articles'][i]['urlToImage']
+        news.publishedAt = jsonData['articles'][i]['publishedAt']
+        # print (i)
+        return news
+        i=i+1
 
 class Everything(models.Model):
     name = models.CharField(max_length=50)
